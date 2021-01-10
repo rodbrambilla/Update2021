0a1e7dc269bc90dd08e4c5d44b31a07bc77bc8cb
Rodrigo <rodbrambilla@gmail.com>
2021-01-10 20:22:16 -0300

    Segundo commit

---
+++ SalesWebMvc/Controllers/DepartmentsController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SalesWebMvc.Models;
+
+namespace SalesWebMvc.Controllers
+{
+    public class DepartmentsController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Department> list = new List<Department>();
+            list.Add(new Department(1, "Eletronics"));
+            list.Add(new Department(2, "Fashion"));
+            
+            return View(list);
+        }
+    }
+}
